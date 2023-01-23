3438bf5d4b8d9d2f2fbada5ba992aac49b3d967f
ShahariarRahman <shahariarrahman98@gmail.com>
2023-01-23 18:01:17 +0600

    user sign in complete then navigate to home, logout from firebase

--- src/layout/main/Navbar.js
+++ src/layout/main/Navbar.js
@@ -1,10 +1,18 @@
 import { signOut } from "firebase/auth";
 import React from "react";
+import { useSelector } from "react-redux";
 
 import { Link, useLocation } from "react-router-dom";
+import auth from "../../firebase/firebase.config";
 
 const Navbar = () => {
   const { pathname } = useLocation();
+
+  const { email } = useSelector((state) => state.auth);
+
+  const handleSignOut = () => {
+    signOut(auth);
+  };
 
   return (
     <nav
@@ -12,24 +20,33 @@ const Navbar = () => {
         pathname === "/" ? null : "bg-white"
       }`}
     >
-      <ul className='max-w-7xl mx-auto flex gap-3 h-full items-center'>
-        <li className='flex-auto font-semibold text-2xl'>
-          <Link to='/'>JobBox</Link>
+      <ul className="max-w-7xl mx-auto flex gap-3 h-full items-center">
+        <li className="flex-auto font-semibold text-2xl">
+          <Link to="/">JobBox</Link>
         </li>
         <li>
-          <Link className='hover:text-primary' to='/jobs'>
+          <Link className="hover:text-primary transition-all" to="/jobs">
             Jobs
           </Link>
         </li>
 
-        <li>
-          <Link
-            className='border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all '
-            to='/login'
+        {email ? (
+          <button
+            onClick={handleSignOut}
+            className=" hover:text-primary transition-all "
           >
-            Login
-          </Link>
-        </li>
+            Log Out
+          </button>
+        ) : (
+          <li>
+            <Link
+              className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all "
+              to="/login"
+            >
+              Login
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { useNavigate } from "react-router-dom";
 import loginImage from "../assets/login.svg";
 import { loginUser } from "../features/auth/authSlice";
 const Login = () => {
+  const { isLoading, email } = useSelector((state) => state.auth);
   const { register, handleSubmit, reset } = useForm();
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -13,6 +15,12 @@ const Login = () => {
   const onSubmit = ({ email, password }) => {
     dispatch(loginUser({ email, password }));
   };
+
+  useEffect(() => {
+    if (!isLoading && email) {
+      navigate("/");
+    }
+  }, [isLoading, email]);
 
   return (
     <div className="flex h-screen items-center">
