81a6e4b46a0f7f4cbd8d01114667012969948b68
ShahariarRahman <shahariarrahman98@gmail.com>
2023-01-23 21:16:53 +0600

    RTK Query store setup Done, and environment variable as baseUrl

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "../features/auth/authSlice";
-
+import apiSlice from "../features/api/apiSlice";
 const store = configureStore({
   devTools: true,
   reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export default store;
