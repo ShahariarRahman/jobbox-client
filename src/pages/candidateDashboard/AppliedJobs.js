1cc5a5fd4d4c78e25de6816b5eac0b2b7c6ae8c8
ShahariarRahman <shahariarrahman98@gmail.com>
2023-01-27 19:37:42 +0600

    applied job has shown to candidate

--- src/pages/candidateDashboard/AppliedJobs.js
+++ src/pages/candidateDashboard/AppliedJobs.js
@@ -13,13 +13,13 @@ const AppliedJobs = () => {
   if (isLoading) {
     return <Loading />;
   }
-
+  console.log(data);
   return (
     <div>
-      <h1 className='text-xl py-5'>Applied jobs</h1>
-      <div className='grid grid-cols-2 gap-5 pb-5'>
-        {data?.data?.map((job) => (
-          <JobCard jobData={job} />
+      <h1 className="text-xl py-5">Applied jobs</h1>
+      <div className="grid grid-cols-2 gap-5 pb-5">
+        {data?.data?.map((job, i) => (
+          <JobCard key={i} jobData={job} />
         ))}
       </div>
     </div>
